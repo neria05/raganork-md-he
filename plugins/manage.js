8667ae7678b746acc4498a54356a373045c630d4
Neria - Ner online <68824143+neria05@users.noreply.github.com>
2024-06-28 04:55:39 +0300

    lang error

--- plugins/manage.js
+++ plugins/manage.js
@@ -191,7 +191,7 @@ fs.writeFileSync('./config.env', lines.join('\n'));
         desc: "Change bot's language for some commands",
         use: 'settings'
     }, (async (message, match) => {
-      if (!match[1] || !["english","manglish","turkish"].includes(match[1].toLowerCase())) return await message.sendReply("_Invalid language! Available languages are English, Manglish and Turkish_");  
+      if (!match[1] || !["english","manglish","turkish","heb"].includes(match[1].toLowerCase())) return await message.sendReply("_שפה שנבחרה לא זמינה_");  
       return await setVar("LANGUAGE",match[1].toLowerCase(),message)
     }));
     Module({
